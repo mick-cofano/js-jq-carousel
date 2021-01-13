039d198bd438d7ec12d5b76428f15874f4fc9bfa
Michele Cofano <brutalmachinelol@gmail.com>
2021-01-13 17:58:27 +0100

    prova n.2

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-// Prima versione, ok ma non riesco ad eseguire un loop
+// // Prima versione, ok ma non riesco ad eseguire un loop
+// $(document).ready(function() {
+//
+//   $('.next').click(function() {
+//     $('img.active').removeClass('active').next().addClass('active');
+//   })
+//
+//   $('.prev').click(function() {
+//     $('img.active').removeClass('active').prev().addClass('active');
+//   })
+//
+// })
+
 $(document).ready(function() {
 
   $('.next').click(function() {
     $('img.active').removeClass('active').next().addClass('active');
+    $('img:last-of-type').addClass('active');
   })
 
   $('.prev').click(function() {
     $('img.active').removeClass('active').prev().addClass('active');
+    $('img:first-child').addClass('active');
   })
 
 })
